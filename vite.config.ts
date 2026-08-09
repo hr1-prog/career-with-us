cf19679c9c13283375d60769b1129400287234d6
hr1-prog <hr1@careerwithus.co.in>
2026-08-09 15:43:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -162,8 +162,8 @@ export default defineConfig({
     },
   },
   envDir: path.resolve(import.meta.dirname),
-  root: path.resolve(import.meta.dirname, "client"),
-  publicDir: path.resolve(import.meta.dirname, "client", "public"),
+  root: path.resolve(import.meta.dirname),
+  publicDir: path.resolve(import.meta.dirname,
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
